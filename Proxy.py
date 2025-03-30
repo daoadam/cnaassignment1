dc863e71b2632ce9843b372dc9f91a772569caf2
daoadam <adamdao.xd@gmail.com>
2025-03-30 19:03:15 +1030

    enable proxy to accept client connections with address

--- Proxy.py
+++ Proxy.py
@@ -61,7 +61,7 @@ while True:
   # Accept connection from client and store in the clientSocket
   try:
     # ~~~~ INSERT CODE ~~~~
-    
+    clientSocket, addr = serverSocket.accept()
     # ~~~~ END CODE INSERT ~~~~
     print ('Received a connection')
   except:
